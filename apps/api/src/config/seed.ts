da483a0ee0b05fc882b0d5499e907dca74cb897a
Ricardo Charahua Sánchez <ricardo.charahua@ucsm.edu.pe>
2026-07-04 18:06:30 -0500

    agregando 2 cuentas

--- apps/api/src/config/seed.ts
+++ apps/api/src/config/seed.ts
@@ -4,6 +4,8 @@ import { prisma } from './prisma'
 
 async function seed() {
   const hash = await bcrypt.hash('admin123', 10)
+  const hekartHash = await bcrypt.hash('adminhekart', 10)
+  const gladisHash = await bcrypt.hash('admingladis', 10)
 
   await prisma.gEN_USR.upsert({
     where:  { ID_USR: '00000001' },
@@ -13,6 +15,30 @@ async function seed() {
       NOM_USR:   'Administrador',
       EMAIL_USR: 'admin@bachanetto.com',
       PWD_USR:   hash,
+      ROL_USR:   'ADMIN',
+    },
+  })
+
+  await prisma.gEN_USR.upsert({
+    where:  { ID_USR: '11111111' },
+    update: {},
+    create: {
+      ID_USR:    '11111111',
+      NOM_USR:   'Admin Hekart',
+      EMAIL_USR: 'adminhekart@bachanetto.com',
+      PWD_USR:   hekartHash,
+      ROL_USR:   'ADMIN',
+    },
+  })
+
+  await prisma.gEN_USR.upsert({
+    where:  { ID_USR: '22222222' },
+    update: {},
+    create: {
+      ID_USR:    '22222222',
+      NOM_USR:   'Admin Gladis',
+      EMAIL_USR: 'admingladis@bachanetto.com',
+      PWD_USR:   gladisHash,
       ROL_USR:   'ADMIN',
     },
   })
